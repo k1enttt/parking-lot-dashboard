afc68c271091bb9386cb9fe8a6621366cabca98e
Tạ Thúc Trung Kiên <tathuctrungkien2003@gmail.com>
2025-03-20 09:36:24 +0700

    Refactor

--- app/page.tsx
+++ app/page.tsx
@@ -8,15 +8,11 @@ import { createClient } from "@/utils/supabase/server";
 export default async function Home() {
   const supabase = await createClient();
   const cards = await getAllCards(supabase) as Card[];
-  const cardKien = await getCardByCode(supabase, 'kien123')
   const carSlots = await getAllCarSlots(supabase) as CarSlot[];
-  const carSlot = await getCarSlotById(supabase, 1)
-  const update = await updateCarSlotIsEmpty(supabase, 1, true)
 
 
   return (
     <div className="flex items-start justify-center gap-10">
-      <pre>{JSON.stringify(carSlots, null, 2)}</pre>
       <CardTable data={cards} />
       <CarSlotTable data={carSlots} />
     </div>
